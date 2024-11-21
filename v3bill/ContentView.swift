f923d441bedcd5d8a3a763df6a3524436f016301
wes <malmowes@hotmail.com>
2024-11-21 09:12:53 +0100

    Initial Commit

---
+++ v3bill/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  v3bill
+//
+//  Created by applwes on 2024-11-21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ v3bill/v3billApp.swift
@@ -0,0 +1,17 @@
+//
+//  v3billApp.swift
+//  v3bill
+//
+//  Created by applwes on 2024-11-21.
+//
+
+import SwiftUI
+
+@main
+struct v3billApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
